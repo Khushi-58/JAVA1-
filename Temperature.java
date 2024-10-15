89db4952b7dc2daf6f95eaf60da2d348fc32dd24
Khushi K <169142716+Khushi-58@users.noreply.github.com>
2024-10-15 15:39:11 +0530

    celsius to Fahrenheit

---
+++ Temperature.java
@@ -0,0 +1,9 @@
+
+public class Temperature {
+    public static void main(String[]args){
+        float tempC,tempF;
+        tempC=30;
+        tempF=((tempC*9)/5)+32;
+        System.out.println("Temperature is "+tempF);
+    }    
+}
