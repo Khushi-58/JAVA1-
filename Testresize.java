2198923738c57ed2f4811ffdef7d0e22a2e8365a
Khushi K <169142716+Khushi-58@users.noreply.github.com>
2024-10-29 14:41:37 +0530

    Add files via upload

---
+++ Testresize.java
@@ -0,0 +1,41 @@
+package pkg1;
+interface Resizable1{
+    void resizeWidth(int width);
+    void resizeHeight(int height);
+class Rectangle implements Resizable1{
+    private int width;
+    private int height;
+    public Rectangle(int width,int height){
+        this.width=width;
+        this.height=height;
+    }
+    public void resizeWidth(int width){
+        this.width=width;
+    }
+    public void resizeHeight(int height){
+        this.height=height;
+    }
+    public void display(){
+        System.out.println("Rectangle Width:"+width);
+        System.out.println("Rectangle Height:"+height);
+    }
+    
+}
+
+public class Testresize {
+    public static void main(String[] args){
+        String title0="Lab 7 program-Resizable Interface";
+        String title1="Develop a Java program to create an interface resizable with method resizeWidth(int width) and resizeHeight(int height)that allow an object to be resized";
+        String title2="create a class rectangle that implements the resizable interface and implements the resize methods";
+        System.out.println(title0+"\n"+title1+"\n"+title2);
+        Rectangle r1=new Rectangle(5,10);
+        System.out.println("\nOriginal Rectangle:");
+        r1.display();
+        r1.resizeWidth(8);
+        r1.resizeHeight(12);
+        System.out.println("\n Resized Rectangle:");
+        r1.display();
+    }
+}
+    
+}
