2477cade56e8b058ccd2df78ae440ba9fdc69276
Khushi K <169142716+Khushi-58@users.noreply.github.com>
2024-11-05 15:46:23 +0530

    file with names of student

---
+++ Testfile.java
@@ -0,0 +1,20 @@
+package pkg1;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Testfile {
+    public static void main(String[] args)throws FileNotFoundException{
+        File f1=new File("name.txt");
+        Scanner sc1=new Scanner(f1);
+        String name=" ";
+        Tools t1=new Tools();
+        name=sc1.nextLine();
+        System.out.println(t1.reverse(name));
+        name=sc1.nextLine();
+        System.out.println(t1.reverse(name));
+        name=sc1.nextLine();
+        System.out.println(t1.reverse(name));
+    }
+    
+}
