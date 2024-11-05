3beb10b6dd09ae08167cbafc3a5b876392303d08
Khushi K <169142716+Khushi-58@users.noreply.github.com>
2024-11-05 15:47:46 +0530

    create for loop to read the first 6 lines from the name.txt

---
+++ Testfile2.java
@@ -0,0 +1,15 @@
+package pkg1;
+import java.io.File;
+import java.util.Scanner;
+
+public class Testfile2 {
+    public static void main(String[] args){
+        String name=" ";
+        Tools t1=new Tools();
+        for(int i=0;i<6;i++){
+             System.out.println(t1.reverse("name"));
+        }
+    }
+}
+        
+
