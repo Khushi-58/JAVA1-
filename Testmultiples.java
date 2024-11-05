13a185263b46dd819d2be58a1b983e777662d6b4
Khushi K <169142716+Khushi-58@users.noreply.github.com>
2024-11-05 15:37:54 +0530

    even and odd numbers

---
+++ Testmultiples.java
@@ -0,0 +1,20 @@
+package pkg1;
+public class Testmultiples {
+    public static void main(String[] args){
+        for(int i=0;i<=5;i++){
+            System.out.println(i*2+" ");
+        }
+        System.out.println("");
+        for(int i=0;i<=5;i++){
+            System.out.println(i*2+1+" ");
+        }
+        System.out.println("");
+        for(int j=3;j<101;j++){
+            for(int i=0;i<=10;i++){
+                System.out.println(i*j+" ");
+            }
+            System.out.println();
+            }
+        }
+    }
+    
