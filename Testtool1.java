5daeb3c729f5bd758a0148ecc9f45a544395240e
Khushi K <169142716+Khushi-58@users.noreply.github.com>
2024-11-05 15:44:44 +0530

    class created as a separate file in diff package

---
+++ Testtool1.java
@@ -0,0 +1,9 @@
+package pkg1;
+import pkg1.Tools;
+public class Testtool1 {
+    public static void main(String[] args){
+        Tools t1=new Tools();
+        System.out.println(t1.reverse("Khushi"));
+    }
+    
+}
