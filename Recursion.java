458a4d1db948407ae68a97e8b80a1e5c05b9e4c8
Khushi K <169142716+Khushi-58@users.noreply.github.com>
2024-11-12 15:59:19 +0530

    factorial of a number

---
+++ Recursion.java
@@ -0,0 +1,16 @@
+class Factorial{
+    int fact(int n){
+        if(n==1)
+            return 1;
+        int result=fact(n-1)*n;
+        return result;  
+}
+}
+public class Recursion {
+    public static void main(String args[]){
+        Factorial f=new Factorial();
+        System.out.println("FACTORIAL OF 3 is "+ f.fact(3));
+        System.out.println("FACTORIAL OF 4 is "+ f.fact(4));
+    }
+    
+}
